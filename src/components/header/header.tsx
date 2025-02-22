bb27749451e71a948de14aeefd01c53a16909149
creamlaflare <antonkazachenko2002@gmail.com>
2025-02-21 16:33:12 -0800

    create animations for header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,27 +1,52 @@
 import React, { FC } from 'react';
-import '../../index.css';
+import { NavLink } from 'react-router-dom';
 import { ReactComponent as GithubLogo } from '../../assets/images/githubLogo.svg';
 import styles from './header.module.css';
 
 const Header: FC<object> = () => (
   <header className={styles.headerFlex}>
     <div className={styles.navBar}>
-      <nav className={styles.logo}>
+      {/* eslint-disable-next-line react/jsx-no-undef */}
+      <NavLink to="/" replace className={`${styles.hoverText} ${styles.logo}`} data-text="LAVADATA">
         LAVADATA
-      </nav>
+      </NavLink>
       <div className={styles.separator} />
       <div className={styles.navMiddle}>
-        <nav>
+        <NavLink to="/" className={styles.hoverText} data-text="Categories">
           Categories
-        </nav>
-        <nav>
+        </NavLink>
+        <NavLink to="/" className={styles.hoverText} data-text="About">
           About
-        </nav>
+        </NavLink>
       </div>
       <div className={styles.separator} />
       <div className={styles.githubLogo}>
-        <a href="https://github.com/antonkazachenko/lava-data" target="_blank" rel="noopener noreferrer">
-          <GithubLogo />
+        <a
+          href="https://github.com/antonkazachenko/lava-data"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <svg
+            width="18"
+            height="21"
+            viewBox="0 0 18 21"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <defs>
+              <radialGradient id="githubGradient" cx="50%" cy="50%" r="50%">
+                <stop offset="0%" stopColor="#FF9900" />
+                <stop offset="100%" stopColor="#C31700" />
+              </radialGradient>
+            </defs>
+            <g className={styles.solid}>
+              <GithubLogo stroke="white" strokeWidth="1.5" />
+            </g>
+            <g className={styles.gradient}>
+              <GithubLogo stroke="url(#githubGradient)" strokeWidth="1.5" />
+            </g>
+          </svg>
+
         </a>
       </div>
     </div>
