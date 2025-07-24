306b5d3104b51408d6b3e5d0928473967e16256e
creamlaflare <antonkazachenko2002@gmail.com>
2025-07-24 08:17:04 -0700

    update css

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,11 +1,32 @@
 import React, { FC } from 'react';
+import styles from './about.module.css';
 
 const AboutComponent: FC = () => (
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae delectus, deserunt
-    dolorem eveniet itaque maxime, nostrum, quia sequi temporibus totam voluptate. Ad harum,
-    quisquam. Blanditiis dolores quae sunt ullam!
-  </p>
+  <div>
+    <div className={styles.mainText}>
+      <h3>Overview</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium aliquid animi,
+        assumenda commodi consectetur delectus deserunt dicta doloremque ea eos ex excepturi,
+        facilis inventore iure labore laboriosam laborum laudantium modi molestias neque odit
+        officia officiis omnis quo quos similique soluta sunt, unde veniam voluptas voluptate
+        voluptatem voluptates? Accusantium asperiores deleniti esse incidunt necessitatibus,
+        omnis? A accusantium, beatae consequuntur delectus dolorem eligendi esse est et ex
+        expedita libero molestiae natus necessitatibus perferendis quae quas quod rem suscipit
+        tempora totam ullam velit voluptatum. Alias animi blanditiis commodi deserunt dolor
+        dolorem fugit, illum in, maxime, necessitatibus neque officia placeat quis ratione soluta.
+      </p>
+    </div>
+    <div className={styles.link}>
+      <a
+        href="https://www.nickmvincent.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Link
+      </a>
+    </div>
+  </div>
 );
 
 export default AboutComponent;
