4a1344f7b89c81ccfd238be73f997bc550dc9350
creamlaflare <antonkazachenko2002@gmail.com>
2025-07-29 12:37:40 -0700

    reposition the speed slider

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -31,6 +31,7 @@ const HomePage: FC = () => {
         <span>Speed</span>
         <Slider.Root
           className={styles.SliderRoot}
+          orientation="vertical"
           defaultValue={[50]}
           max={100}
           step={1}
